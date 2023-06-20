5af35c9e71b4b67132f4032fb878462ad2cefc7f
yohannesmaru <yohannesmaru21@gmail.com>
2023-06-20 04:13:23 -0700

    Adding 5-sign

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-    int r;
+	int r;
 
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
+	r = print_sign(98);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
 	_putchar('\n');
 	r = print_sign(0);
 	_putchar(',');
